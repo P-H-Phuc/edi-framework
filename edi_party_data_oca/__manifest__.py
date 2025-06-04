cdc8f6ab353b0b6a6ba8fc16b803a953348caef6
Simone Orsi <simahawk@gmail.com>
2025-06-04 07:52:44 +0200

    edi_party_data: migrate to v18

--- edi_party_data_oca/__manifest__.py
+++ edi_party_data_oca/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
     Allow to configure and retrieve party information for EDI exchanges.
     """,
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "development_status": "Alpha",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/edi-framework",
