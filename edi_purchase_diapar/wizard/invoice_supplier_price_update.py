2370ac2366a018143c871be75e911527df832419
phucph <phucph@trobz.com>
2025-12-17 09:48:32 +0700

    [IMP] edi_purchase_diapar: pre-commit auto fixes

--- edi_purchase_diapar/wizard/invoice_supplier_price_update.py
+++ edi_purchase_diapar/wizard/invoice_supplier_price_update.py
@@ -5,38 +5,37 @@
 from openerp import api, fields, models
 
 SUPPORTED_MODELS = [
-    'account.invoice',
-    'purchase.order',
+    "account.invoice",
+    "purchase.order",
 ]
 
 
 class InvoiceSupplierPriceUpdate(models.TransientModel):
-    _name = 'invoice.supplier.price.update'
-    _description = 'Invoice Supplier Price Update'
+    _name = "invoice.supplier.price.update"
+    _description = "Invoice Supplier Price Update"
 
     show_discount = fields.Boolean()
-    partner_id = fields.Many2one(comodel_name='res.partner')
+    partner_id = fields.Many2one(comodel_name="res.partner")
     edi_line_ids = fields.One2many(
-        comodel_name='supplier.info.update.line',
-        inverse_name='inv_supplier_price_id'
+        comodel_name="supplier.info.update.line", inverse_name="inv_supplier_price_id"
     )
 
     @api.model
     def default_get(self, fields_list):
-        res = super(InvoiceSupplierPriceUpdate, self).default_get(
-            fields_list=fields_list)
-        active_model = self._context.get('active_model', '')
-        active_id = self._context.get('active_id', False)
+        res = super().default_get(fields_list=fields_list)
+        active_model = self._context.get("active_model", "")
+        active_id = self._context.get("active_id", False)
         if active_model in SUPPORTED_MODELS and active_id:
             active_obj = self.env[active_model].browse(active_id)
-            processed_lines = self.compute_process_lines(
-                active_model, active_obj)
+            processed_lines = self.compute_process_lines(active_model, active_obj)
             show_discount = active_obj.partner_id.show_discount
-            res.update({
-                'edi_line_ids': processed_lines,
-                'partner_id': active_obj.partner_id.id,
-                'show_discount': show_discount,
-            })
+            res.update(
+                {
+                    "edi_line_ids": processed_lines,
+                    "partner_id": active_obj.partner_id.id,
+                    "show_discount": show_discount,
+                }
+            )
         return res
 
     @api.model
@@ -50,40 +49,45 @@ class InvoiceSupplierPriceUpdate(models.TransientModel):
         """
         lines = []
         partner_id = active_obj.partner_id
-        obj_lines = 'order_line' in active_obj and active_obj.order_line \
-                    or active_obj.invoice_line_ids
+        obj_lines = (
+            "order_line" in active_obj
+            and active_obj.order_line
+            or active_obj.invoice_line_ids
+        )
         # For invoice date
-        supplier_price_list_obj = self.env['supplier.price.list']
-        price_date = active_model == 'account.invoice' \
-            and active_obj.date_invoice or False
+        supplier_price_list_obj = self.env["supplier.price.list"]
+        price_date = (
+            active_model == "account.invoice" and active_obj.date_invoice or False
+        )
         # END
         for line in obj_lines:
             product_id = line.product_id
             selected_seller_id = product_id.seller_ids.filtered(
-                lambda seller: seller.name == partner_id and
-                seller.price_policy == line.price_policy
+                lambda seller: seller.name == partner_id
+                and seller.price_policy == line.price_policy
             )
-            selected_seller_id = \
-                selected_seller_id and selected_seller_id[0] or False
+            selected_seller_id = selected_seller_id and selected_seller_id[0] or False
             if selected_seller_id:
                 seller_price_unit = selected_seller_id.base_price
                 seller_discount = selected_seller_id.discount
                 seller_price_policy = selected_seller_id.price_policy
-                linked_line_key = \
-                    active_model == 'purchase.order' and 'po_line_id' \
-                    or 'invoice_line_id'
+                linked_line_key = (
+                    active_model == "purchase.order"
+                    and "po_line_id"
+                    or "invoice_line_id"
+                )
                 seller_values = {
-                    'product_id': product_id.id,
-                    'price_policy': seller_price_policy,
-                    'supplier_price_unit': seller_price_unit,
-                    'supplier_discount': seller_discount,
-                    'show_discount': partner_id.show_discount,
+                    "product_id": product_id.id,
+                    "price_policy": seller_price_policy,
+                    "supplier_price_unit": seller_price_unit,
+                    "supplier_discount": seller_discount,
+                    "show_discount": partner_id.show_discount,
                     linked_line_key: line.id,
-                    'seller_id': selected_seller_id.id,
+                    "seller_id": selected_seller_id.id,
                 }
                 line_price_unit = line.price_unit
-                if 'discount' in line:
-                    line_discount =  line.discount
+                if "discount" in line:
+                    line_discount = line.discount
                 else:
                     seller_discount
 
@@ -91,30 +95,31 @@ class InvoiceSupplierPriceUpdate(models.TransientModel):
                 if partner_id.is_edi:
                     product_code = selected_seller_id.product_code
                     price_domain = []
-                    price_domain.append(('supplier_id', '=', partner_id.id))
-                    price_domain.append(('supplier_code', '=', product_code))
+                    price_domain.append(("supplier_id", "=", partner_id.id))
+                    price_domain.append(("supplier_code", "=", product_code))
                     if price_date:
-                        price_domain.append(('apply_date', '<=', price_date))
+                        price_domain.append(("apply_date", "<=", price_date))
                     edi_price = supplier_price_list_obj.search(
-                        price_domain,
-                        order="apply_date desc"
+                        price_domain, order="apply_date desc"
                     )
                     if edi_price:
                         line_price_unit = edi_price[0].price
                 # End
                 # Prepare values in current document line
-                seller_values.update({
-                    'price_unit': line_price_unit,
-                    'discount': line_discount,
-                })
+                seller_values.update(
+                    {
+                        "price_unit": line_price_unit,
+                        "discount": line_discount,
+                    }
+                )
                 lines.append((0, 0, seller_values))
         return lines
 
     @api.multi
     def update_prices_second(self):
         self.ensure_one()
-        active_model = self._context.get('active_model', '')
-        active_id = self._context.get('active_id', False)
+        active_model = self._context.get("active_model", "")
+        active_id = self._context.get("active_id", False)
         active_obj = self.env[active_model].browse(active_id)
         lines = self.edi_line_ids.sorted()
         obj_lines_values = []
@@ -122,36 +127,35 @@ class InvoiceSupplierPriceUpdate(models.TransientModel):
             update_values = {}
             supplier_price_unit = line.supplier_price_unit
             updated_price_unit = line.price_unit
-            if updated_price_unit > 0 and \
-                    updated_price_unit != supplier_price_unit:
-                update_values['base_price'] = updated_price_unit
+            if updated_price_unit > 0 and updated_price_unit != supplier_price_unit:
+                update_values["base_price"] = updated_price_unit
             supplier_discount = line.supplier_discount
             updated_discount = line.discount
             if updated_discount != supplier_discount:
-                update_values['discount'] = updated_discount
+                update_values["discount"] = updated_discount
             if update_values:
                 seller_id = line.seller_id
                 product_template = seller_id.product_tmpl_id
                 seller_id.write(update_values)
                 product_template.auto_update_theoritical_cost_price()
-            if active_model == 'account.invoice':
-                obj_lines_values.append((1, line.invoice_line_id.id, {
-                    'price_unit': updated_price_unit
-                }))
-            elif active_model == 'purchase.order':
-                obj_lines_values.append((1, line.po_line_id.id, {
-                    'price_unit': updated_price_unit
-                }))
-        if active_model == 'account.invoice':
-            active_obj.write({'invoice_line_ids': obj_lines_values})
-        elif active_model == 'purchase.order':
-            active_obj.write({'order_line': obj_lines_values})
+            if active_model == "account.invoice":
+                obj_lines_values.append(
+                    (1, line.invoice_line_id.id, {"price_unit": updated_price_unit})
+                )
+            elif active_model == "purchase.order":
+                obj_lines_values.append(
+                    (1, line.po_line_id.id, {"price_unit": updated_price_unit})
+                )
+        if active_model == "account.invoice":
+            active_obj.write({"invoice_line_ids": obj_lines_values})
+        elif active_model == "purchase.order":
+            active_obj.write({"order_line": obj_lines_values})
         return True
 
 
 class SupplierInfoUpdateLine(models.TransientModel):
-    _inherit = 'supplier.info.update.line'
+    _inherit = "supplier.info.update.line"
 
     inv_supplier_price_id = fields.Many2one(
-        comodel_name='invoice.supplier.price.update'
+        comodel_name="invoice.supplier.price.update"
     )
