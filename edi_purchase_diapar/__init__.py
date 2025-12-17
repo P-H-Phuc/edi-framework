3b10ce7c6e1724ae821bb5c9aad21a3fd44ff243
phucph <phucph@trobz.com>
2025-12-17 10:46:46 +0700

    [ADD] edi_purchase_diapar_oca

--- edi_purchase_diapar/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2016-Today: Druidoo (<http://www.druidoo.io/>)
-# @author: Druidoo
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
-
-from . import models
-from . import wizard
